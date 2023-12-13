3a1801ae68de365d617ba0a47b90e5986e6670e7
roxycg <147172968+roxycg@users.noreply.github.com>
2023-12-13 10:45:52 +0000

    Adding ticket 1 and terraform changes

--- src/__init__.py
+++ src/__init__.py
@@ -1,34 +1,36 @@
 #define a function here
 
-#def read_csv():
+def read_csv(csvfile):
     #creating an empty list to append csv to
-import pandas as pd
-import csv
-count_row = 0
-with open('results.csv', newline='') as csvfile:
-    for row in csvfile:
-        row_list= row.strip().split(',')
-        if count_row == 0:
-             #df = pd.Dataframe({})
-             column_headers = row_list
-             length =len(row_list)
-             df = pd.DataFrame(index = range(0), columns= column_headers)
-             print(row_list)
-             #print(df)
-             count_row = count_row + 1
-        else:
-           # df = df.append(pd.Series(row_list, index = column_headers), ignore_index=True)
-           # df = pd.concat([df, pd.Series(row_list, index = column_headers]), ignore_index=True)
-            # using loc methods
-            df.loc[len(df)] = row_list
-            #print(df)
-    print(df)
+    import pandas as pd
+    import csv
+    count_row = 0
+    with open(csvfile, newline='') as csvfile:
+        for row in csvfile:
+            row_list= row.strip().split(',')
+            if count_row == 0:
+                 #df = pd.Dataframe({})
+                 column_headers = row_list
+                 length =len(row_list)
+                 df = pd.DataFrame(index = range(0), columns= column_headers)
+                 print(row_list)
+                 #print(df)
+                 count_row = count_row + 1
+            else:
+                # using loc methods
+                df.loc[len(df)] = row_list
+                #print(df)
+        return df
 
+#running function - should be moved to other file
+df = read_csv('results.csv')
+print(df)
 
-#import csv
-#with open('eggs.csv', newline='') as csvfile:
-#    spamreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
-#    for row in spamreader:
-#        print(', '.join(row))
+#remove duplicate entries based on user_id field
+column_names = list(df.columns)
+print(column_names)
+#df_dededuped = df.drop_duplicates(subset=)
+#print(df_dededuped)
+#keeps first duplicate by default
 
 
